d60dbd2c54258d6a244057700311e0107ba4925a
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-14 15:06:47 -0400

    added servcies cards

--- src/components/GymServices.tsx
+++ src/components/GymServices.tsx
@@ -1,9 +1,49 @@
+import dumbell from '../../public/images/weight.png'
+import { PreloadStaticImage } from './PreloadImage';
+import Image from 'next/image';
 
+type GymServicesProps =
+    {
+        title: string;
+        description: string;
+    }
 
-export default function GymService() {
+export const GymService: React.FC<{ services: GymServicesProps[] }> = ({ services }) => {
     return (
         <>
-
+            {services ? (
+                <>
+                    {services.map(({ title, description }) => {
+                        return (
+                            <div className='grid grid-cols-1 rounded-lg p-9 w-full ease-out duration-1000 delay-1000 cardShadow border-slate-700'>
+                                <div className='flex place-content-center'>
+                                    <Image
+                                        src={dumbell}
+                                        alt=''
+                                        height={70}
+                                        width={70}
+                                    />
+                                </div>
+                                <div className='text-center text-2xl mb-2'>
+                                    {title}
+                                </div>
+                                <div className='text-center'>
+                                    {description}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </>
+            ) :
+                (
+                    <>
+                        <div>
+                            Uh Oh!
+                        </div>
+                    </>
+                )}
         </>
     )
 }
+
+export type { GymServicesProps };
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,6 +9,7 @@ import { SubscribeContext } from "@/lib/SubscribeContext";
 import ToastMessage from "./ToastMessage";
 import { PreloadStaticImage } from "./PreloadImage";
 import mainWorkoutImage from '../../public/images/deadLift.webp'
+import { GymService, GymServicesProps } from "./GymServices";
 
 export const HomePage: React.FC<{ blogPosts: BlogPostData[] }> = ({ blogPosts }) => {
     const { showModal, setShowModal, showSuccessMessage } = useContext(SubscribeContext)
@@ -19,6 +20,10 @@ export const HomePage: React.FC<{ blogPosts: BlogPostData[] }> = ({ blogPosts })
         <>
             <div className="relative justify-center items-center mx-2 lg:mx-40 mt-2 drop-shadow-1xl">
                 <PreloadStaticImage imgSrc={mainWorkoutImage} styleProps={'h-[700px] diagonal-cut-image'} />
+
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5">
+                    <GymService services={gymServices} />
+                </div>
                 {/* <div className="flex w-full justify-center">
                     {showModal && (
                         <Subscribe closeModal={closeModal} />
@@ -43,3 +48,19 @@ export const HomePage: React.FC<{ blogPosts: BlogPostData[] }> = ({ blogPosts })
         </>
     )
 }
+
+const gymServices: GymServicesProps[] = [
+
+    {
+        title: 'High-Intensity Workouts',
+        description: 'Our expert coaches guide you through functional movements, strength training, and conditioning exercises.'
+    },
+    {
+        title: 'Functional Equipment',
+        description: 'Experience functional fitness at its best with our cutting-edge equipment and personalized training'
+    },
+    {
+        title: 'Flexible Class Schedules',
+        description: 'Our gym offers flexible class times to fit your busy schedule'
+    }
+]
